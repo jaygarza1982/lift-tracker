ae60577493a420e016b93e1aedb63840635f0e4d
Jake Garza <jaygarza1982@gmail.com>
2022-10-07 20:53:07 -0400

    Added category add fields in categories page

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,3 +1,4 @@
+import { useFormik } from 'formik';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -53,6 +54,60 @@ const CategoryLink = ({ category, loadCategories }) => {
     );
 }
 
+const CategoryAddForm = ({ load }) => {
+    
+    const formik = useFormik({
+        initialValues: {
+            categoryName: '',
+        },
+        onSubmit: (values) => {
+            // Cannot add blank exercise name
+            if (values?.categoryName.trim() == '') return;
+
+            const addValues = async () => {
+                try {
+                    // Write to DB
+                    const categories = dbutils.stores.CATEGORIES;
+
+                    await dbutils.utils.write(categories, values);
+        
+                    // Reload categories
+                    load();
+                    formik.resetForm();
+                } catch (error) {
+                    console.log('Could not add category', error);
+                }
+            }
+
+            addValues();
+        }
+    });
+
+    return (
+        <div className="exercise-add-form">
+            <div>
+                <input
+                    className="input is-info"
+                    type="text"
+                    placeholder="Category Name"
+                    name="categoryName"
+                    value={formik.values.categoryName}
+                    onChange={formik.handleChange}
+                    onKeyDown={e => e.key === 'Enter' ? formik.handleSubmit(e) : undefined}
+                />
+            </div>
+            <div>
+                <button
+                    className="button"
+                    onClick={formik.handleSubmit}
+                >
+                    Add
+                </button>
+            </div>
+        </div>
+    );
+}
+
 const Categories = () => {
 
     const [categories, setCategories] = useState([]);
@@ -74,6 +129,7 @@ const Categories = () => {
     return (
         // TODO: Way to add categories from this list like the exercises page
         <div className="exercise-categories">
+            <CategoryAddForm load={loadCategories} />
             {/* List of categories. Clicking on it will bring you to a page where you can add sets of reps like below */}
             <div className="card-grid">
                 {
